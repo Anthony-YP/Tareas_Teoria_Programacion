037b11b83a285d9c9e97b113786bfb9c228bf109
Anthony-YP <anthony.j.yaguana@unl.edu.ec>
2024-06-04 10:27:58 -0500

    Add files via upload

---
+++ Ejercicios_Bucle_Do_While/Ejercicio_12_Do_While.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+// 12. Sumar los números impares hasta n //
+
+int main()
+{
+    int numero, respuesta, contador;
+    printf("- Ingrese el limite de numeros => ");
+    scanf("%d", &numero);
+    printf("====================================================\n");
+
+    // Do While //
+    printf(" ----- Resultado con bucle For -----\n");
+    contador = 1;
+    do
+    {
+        if (contador % 2 != 0)
+        {
+            respuesta += contador;
+            printf(" %d +", contador);
+        }
+        contador++;
+    } while (contador <= numero);
+
+    printf("\nLa suma de todos los numero impares es => %d", respuesta);
+    return 0;
+}
